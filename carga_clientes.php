99b543b4ffe4d85ceeed61e01848c4bbc0ce3867
Jesus Reyes <djrm1204@gmail.com>
2022-03-18 20:30:01 -0400

    Carga de clientes, agregada la clase para manejar el archivo y obtencion de datos aba e itp

--- carga_clientes.php
+++ carga_clientes.php
@@ -1,33 +1,50 @@
 <?php
 
 session_start();
-require_once 'funcion.php';
-if(count($_FILES) && validaExtension()){
-    
-    
+
+require_once 'funciones.php';
+
+if (count($_FILES) && validaExtension()) {
+    // Mueve a directorio subidas
+    $ruta = moverArchivo('subidas/');
     // Validar columnas segun tipo de datos
-    $COLUMMNAS_ABA_PLUS = ['central','equipo','unidad negocio'];
-    $COLUMMNAS_ITP = ['central','movimiento','unidad negocio'];
+    $COLUMMNAS_ABA_PLUS = ['central', 'equipo', 'unidad negocio'];
+    $COLUMMNAS_ITP = ['central', 'movimiento', 'unidad negocio'];
 
-    $excel = PHPExcel_IOFactory::load($_FILES['archivo']['tmp_name']);
-    // Lee hoja 1
-    $excel->setActiveSheetIndex(0);
 
-    // Valida las columnas en fila 1
-    // Verificar el tamaño de la fila
-    // Validar que existan las colummnas en $COLUMMNAS_ABA_PLUS
+    require_once 'excel.php';
 
-    //for($i=1; $i<=$tamFila)
+    $excel = ClientesAba::leerArchivo($ruta);
+    // Valida cantidad de hojas
+    if (ClientesAba::$excel->getSheetCount() !== 2) {
+        // Faltan hojas
+        // Elimina archivo
+        unlink($ruta);
+        setMsgError('Error de hojas. Deben existir hojas con datos de clientes ABA plus y clientes ITP');
+    }
+    // Datos hoja 1
+    $aba_plus = ClientesAba::getSheetAbaPlus();
+    // Datos hoja 2
+    $itp = ClientesAba::getSheetITP();
+    // Arreglos combinados
+    $aba_plus_itp = [...$aba_plus];
+    foreach ($itp as $v) {
+        $aba_plus_itp[] = $v;
+    }
+    // Todas las centrales
+    $centrales = [];
 
-    //numero de filas
+    foreach ($aba_plus_itp as $c) {
+        $centrales[$c['central']][] = $c;
+    }
+
+
+    error_reporting(E_ALL);
     echo '<pre>';
-    print_r($excel->setActiveSheetIndex(0)->getCell ('A1')->getCalculatedValue());
+    print_r(($centrales));
     echo '</pre>';
     exit;
-}else{
+} else {
     // Agregar mensaje de error a variable $_SESSION
-    $_SESSION['error'] = 'Extension de archivo no permitido';
+    setMsgError('Extension de archivo no permitido');
 }
-
-// REDIRECCION DE RESPUESTA
-header('location: /excel2');
