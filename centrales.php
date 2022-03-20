fe33c269cf376ea413495101e4fcdfd9608586ca
Jesus Reyes <djrm1204@gmail.com>
2022-03-19 21:46:57 -0400

    Clase para obtener datos del archivo de las centrales, extiende de clase archivoexcel

---
+++ centrales.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once 'excel.php';
+
+class Centrales extends ArchivoExcel
+{
+    /**
+     * Archivo valido de centrales
+     * @var string
+     */
+    public static $archivo = '';
+
+    /**
+     * Obtiene las centrales del archivo centrales.xlsx
+     * ubicado en la raiz
+     * @return array
+     */
+    public static function getCentrales()
+    {
+        self::setArchivoCentrales();
+        self::leerArchivo(self::$archivo);
+        self::$cols = ['codigo' => 'A', 'nombre' => 'B', 'region' => 'C', 'estado' => 'D'];
+        self::$sheet = self::$excel->setActiveSheetIndex(0);
+        $data = self::getSheetData();
+        $centrales = [];
+        foreach ($data as $c) {
+            $centrales[$c['codigo']] = $c;
+        }
+        ksort($centrales);
+        return $centrales;
+    }
+
+    /**
+     * Selecciona el archivo de centrales
+     */
+    public static function setArchivoCentrales()
+    {
+        if (file_exists('centrales.xlsx')) {
+            self::$archivo = 'centrales.xlsx';
+        } else if (file_exists('centrales.xls')) {
+            self::$archivo = 'centrales.xls';
+        } else {
+            setMsgError('Archivo de centrales no existe, por favor cargar archivo.');
+        }
+    }
+}
