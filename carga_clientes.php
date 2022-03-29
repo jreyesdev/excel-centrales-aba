3d101bcb56e957a5ce6efa97f82836ffe707ac06
Jesus Reyes <djrm1204@gmail.com>
2022-03-29 16:09:19 -0400

    Correccion error de validacion propiedad V

--- carga_clientes.php
+++ carga_clientes.php
@@ -47,37 +47,35 @@ if (count($_FILES) && validaExtension()) {
     foreach ($centrales as $cod => $cen) {
         if (isset($aba_plus_itp[$cod])) {
             $res = resultadoCentral($aba_plus_itp[$cod]);
-            if ($res['V'] !== 'NULL') {
-                $datos[] = [
-                    'A' => trim($cen['codigo']),
-                    'B' => trim($cen['nombre']),
-                    'C' => trim($cen['region']),
-                    'D' => trim($cen['estado']),
-                    'E' => trim($cen['municipio']),
-                    'F' => trim($cen['cant_mdu']),
-                    'G' => trim($cen['puertos']),
-                    'H' => $res['H'],
-                    'I' => $res['I'],
-                    'J' => $res['H'] + $res['I'],
-                    'K' => $res['K'],
-                    'L' => $res['L'],
-                    'M' => $res['K'] + $res['L'],
-                    'N' => trim($cen['puertos']),
-                    'O' => $res['O'],
-                    'P' => $res['P'],
-                    'Q' => $res['O'] + $res['P'],
-                    'R' => $res['R'],
-                    'S' => $res['S'],
-                    'T' => $res['R'] + $res['S'],
-                    'U' => $res['U'],
-                    'V' => trim($cen['segmento']),
-                    'W' => trim($cen['u_nuevos']),
-                    'X' => trim($cen['matriz']),
-                    'Y' => trim($cen['u_nuevos']) + trim($cen['matriz']),
-                    'Z' => trim($cen['estatus']),
-                    'AA' => trim($cen['t_servicio'])
-                ];
-            }
+            $datos[] = [
+                'A' => trim($cen['codigo']),
+                'B' => trim($cen['nombre']),
+                'C' => trim($cen['region']),
+                'D' => trim($cen['estado']),
+                'E' => trim($cen['municipio']),
+                'F' => trim($cen['cant_mdu']),
+                'G' => trim($cen['puertos']),
+                'H' => $res['H'],
+                'I' => $res['I'],
+                'J' => $res['H'] + $res['I'],
+                'K' => $res['K'],
+                'L' => $res['L'],
+                'M' => $res['K'] + $res['L'],
+                'N' => trim($cen['puertos']),
+                'O' => $res['O'],
+                'P' => $res['P'],
+                'Q' => $res['O'] + $res['P'],
+                'R' => $res['R'],
+                'S' => $res['S'],
+                'T' => $res['R'] + $res['S'],
+                'U' => $res['U'],
+                'V' => trim($cen['segmento']),
+                'W' => trim($cen['u_nuevos']),
+                'X' => trim($cen['matriz']),
+                'Y' => intval(trim($cen['u_nuevos'])) + intval(trim($cen['matriz'])),
+                'Z' => trim($cen['estatus']),
+                'AA' => trim($cen['t_servicio'])
+            ];
         }
     }
     // Guarda los datos y devuelve la ruta del archivo guardado
